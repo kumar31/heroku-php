9c177bc1e0854aee1ecdd033964e0a1346972c26
shin1x1 <shin1x1@gmail.com>
2014-06-07 02:17:05 +0900

    add session.php

---
+++ public/session.php
@@ -0,0 +1,18 @@
+<?php
+ini_set('session.save_handler', 'memcached');
+ini_set('session.save_path', getenv('MEMCACHIER_SERVERS'));
+ini_set('memcached.sess_binary', 1);
+ini_set('memcached.sess_sasl_username', getenv('MEMCACHIER_USERNAME'));
+ini_set('memcached.sess_sasl_password', getenv('MEMCACHIER_PASSWORD'));
+
+const SESSION_KEY = 'count';
+
+session_start();
+
+if (empty($_SESSION[SESSION_KEY])) {
+    $_SESSION[SESSION_KEY] = 0;
+}
+
+$_SESSION[SESSION_KEY]++;
+
+var_dump($_SESSION);
